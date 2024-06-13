f6c5e3d1b62a6b375d1a732e745abab63b3b821f
Tahir Mujawar <31164491+hackytam@users.noreply.github.com>
2024-06-13 19:15:19 +0530

    Update subgram.sh

--- subgram.sh
+++ subgram.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-# Bash script for subdomain enumeration and Telegram notifications
+# Define colors
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 11)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 15)
+RESET=$(tput sgr0)  # Reset color
+
+# Default output directory
+DEFAULT_OUTPUT_DIR="subgram_output"
 
 # Function to send notification to Telegram
 send_telegram_notification() {
-    echo "Sending Telegram notification..."
+    echo -e "$WHITE Sending Telegram notification...$RESET"
 
     # Set Telegram bot token and chat ID
     TELEGRAM_BOT_TOKEN="your_bot_token"  # Replace with your bot token
-    TELEGRAM_CHAT_ID="your_chat_id"      # Replace with your chat ID
+    TELEGRAM_CHAT_ID="telegram_chat_id"      # Replace with your chat ID
 
     # Compose message
-    MESSAGE="Subdomain Enumeration Complete for $DOMAIN\nResults are saved in $DOMAIN directory."
+    MESSAGE="Subdomain Enumeration Complete for $DOMAIN. Results are saved in $OUTPUT_DIR directory."
 
     # Send message using Telegram Bot API
     curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$MESSAGE" > /dev/null
@@ -19,48 +28,107 @@ send_telegram_notification() {
     if [ $? -eq 0 ]; then
         echo "Telegram notification sent successfully."
     else
-        echo "Failed to send Telegram notification."
+        echo -e "$RED Failed to send Telegram notification.$RESET"
     fi
 }
 
 # Function to perform subdomain enumeration
 perform_subdomain_enumeration() {
+    # Check if figlet is installed
+    if ! command -v figlet &> /dev/null; then
+        echo "Figlet is not installed. Installing..."
+        # Install figlet
+        sudo apt-get update
+        sudo apt-get install -y figlet
+    else
+        color_code="\033[31m" # Red color code
+        reset_color="\033[0m" # Reset color code
+
+        figlet_text=$(figlet "\$ubgr@m")
+        colored_text="${color_code}${figlet_text}${reset_color}"
+        echo -e "$colored_text"
+
+        echo -e "${YELLOW}# Coded by Tahir Mujawar${RESET}"
+        echo
+    fi
+
     DOMAIN="$1"
-    OUTPUT_DIR="$DOMAIN"
-    mkdir "$OUTPUT_DIR"
-    
-    # Subdomain enumeration using sublist3r
-    echo "Running sublist3r..."
-    sublist3r -d "$DOMAIN" -o "$OUTPUT_DIR/sublist3r.txt" > /dev/null 2 >&1
+    OUTPUT_DIR="$2"
+    mkdir -p "$OUTPUT_DIR"
+
+     #Subdomain enumeration using sublist3r
+    echo -e "$WHITE Scanning $DOMAIN with sublist3r...$RESET"
+    sublist3r -d "$DOMAIN" -o "$OUTPUT_DIR/${DOMAIN}_sublist3r.txt" > /dev/null 2>&1
 
     # Subdomain enumeration using subfinder
-    echo "Running subfinder..."
-    subfinder -d "$DOMAIN" -silent -o "$OUTPUT_DIR/subfinder.txt" 
+    echo -e "$WHITE Scanning $DOMAIN with subfinder...$RESET"
+    subfinder -d "$DOMAIN" -o "$OUTPUT_DIR/${DOMAIN}_subfinder.txt" > /dev/null 2>&1
 
     # Subdomain enumeration using assetfinder
-    echo "Running assetfinder..."
-    assetfinder "$DOMAIN" > "$OUTPUT_DIR/assetfinder.txt" > /dev/null 2 >&1
+    echo -e "$WHITE Scanning $DOMAIN with assetfinder...$RESET"
+    assetfinder -subs-only "$DOMAIN" > "$OUTPUT_DIR/${DOMAIN}_assetfinder.txt" 2> /dev/null
+    
+    cd $OUTPUT_DIR
+    cat $DOMAIN_*.txt | sort -u > all-subdomains.txt
+    
+    echo -e "$YELLOW Scan completed for $DOMAIN. Results saved in $OUTPUT_DIR directory.$RESET"
 
-    # Subdomain enumeration using chaos
-    echo "Running chaos..."
-    chaos -d facebook.com -key 0f64667b-741d-4073-953d-4171116dc42d -silent > "$OUTPUT_DIR/chaos.txt" 
-    
-    echo "Subdomain enumeration completed. Results saved in $OUTPUT_DIR directory."
-    
     # Send Telegram notification if requested
     if [[ "$SEND_NOTIFICATION" == "yes" ]]; then
         send_telegram_notification
     fi
 }
 
-# Main script
-echo "Welcome to Subdomain Enumeration Script! By Tahir Mujawar"
+# Function to display usage
+display_usage() {
+    echo "Usage: $0 -d DOMAIN [-n] [-o OUTPUT_DIR]"
+    echo "Options:"
+    echo "  -d DOMAIN           Domain name for subdomain enumeration"
+    echo "  -n                  Enable Telegram notification (default: no)"
+    echo "  -o OUTPUT_DIR       Specify output directory (default: '$DEFAULT_OUTPUT_DIR')"
+    echo "  -h                  Display this help message"
+}
 
-# Ask user if they want to receive notification
-read -p "Do you want to receive notification upon completion of subdomain enumeration? (yes/no): " SEND_NOTIFICATION
+# Parse command-line options
+while getopts ":d:no:h" opt; do
+    case ${opt} in
+        d )
+            DOMAIN="$OPTARG"
+            ;;
+        n )
+            SEND_NOTIFICATION="yes"
+            ;;
+        o )
+            OUTPUT_DIR="$OPTARG"
+            ;;
+        h )
+            display_usage
+            exit 0
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            display_usage
+            exit 1
+            ;;
+        : )
+            echo "Option -$OPTARG requires an argument." >&2
+            display_usage
+            exit 1
+            ;;
+    esac
+done
 
-# Ask user for the domain name for subdomain enumeration
-read -p "Enter the domain name for subdomain enumeration: " DOMAIN
+# Check if DOMAIN argument is provided
+if [ -z "$DOMAIN" ]; then
+    echo -e "$RED Error: Domain argument missing $RESET"
+    display_usage
+    exit 1
+fi
+
+# Set default OUTPUT_DIR if not provided
+if [ -z "$OUTPUT_DIR" ]; then
+    OUTPUT_DIR="$DEFAULT_OUTPUT_DIR"
+fi
 
 # Perform subdomain enumeration
-perform_subdomain_enumeration "$DOMAIN"
+perform_subdomain_enumeration "$DOMAIN" "$OUTPUT_DIR"
